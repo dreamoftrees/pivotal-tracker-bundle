f7b0f5426f67ea49e0a4d7caa1da9ad0e6ece023
Charlie McKenzie <charlie@dreamoftrees.com>
2013-07-02 10:18:49 +1000

    initial commit

---
+++ DreamoftreesPivotalTrackerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dreamoftrees\PivotalTrackerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DreamoftreesPivotalTrackerBundle extends Bundle
+{
+}
---
+++ Services/PivotalTrackerService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: charlie
+ * Date: 2/07/13
+ * Time: 10:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Dreamoftrees\PivotalTrackerBundle\Services;
+
+
+class PivotalTrackerService {
+
+}
